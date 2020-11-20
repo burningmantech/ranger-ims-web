8eaad4ed6e0d9a677e1ea10f352630588a2eeb53
Wilfredo Sánchez <wsanchez@wsanchez.net>
2020-11-19 16:26:49 -0800

    Add loadFromStorage()

--- src/auth.js
+++ src/auth.js
@@ -1,4 +1,7 @@
+import moment from "moment";
+
 import { createContext } from "react";
+
 
 export class User {
 
@@ -27,12 +30,11 @@ export class User {
 
 export class TestAuthentationSource {
 
-  static timeout = 1000 * 60 * 5;  // 5 minutes
+  static timeout = moment.duration(5, "minutes");
 
   login = async (username, credentials) => {
     if (credentials.password === username) {
-      const expiration = new Date();
-      expiration.setTime(expiration.getTime() + TestAuthentationSource.timeout);
+      const expiration = moment().add(TestAuthentationSource.timeout);
 
       return {
         user: new User(username),
@@ -51,6 +53,10 @@ export class TestAuthentationSource {
 
 export class Authenticator {
 
+  static STORE_KEY_CLASS = "ims.auth.class";
+  static STORE_KEY_USER = "ims.auth.user";
+  static STORE_KEY_EXPIRATION = "ims.auth.expiration";
+
   constructor(source) {
     if (source === undefined) {
       throw new Error("authentication source is not defined");
@@ -64,8 +70,52 @@ export class Authenticator {
     this.expiration = null;
   }
 
+  loadFromStorage = () => {
+    if (this.user !== null) {
+      // Already loaded
+      return;
+    }
+
+    const store = window.localStorage;
+    const sourceClass = store.getItem(Authenticator.STORE_KEY_CLASS);
+
+    if (!sourceClass) {
+      console.log("No stored credentials found.");
+    }
+    else if (sourceClass !== this.source.constructor.name) {
+      console.log("Ignoring stored credentials from class: " + sourceClass);
+    }
+    else {
+      const userJSONText = store.getItem(Authenticator.STORE_KEY_USER);
+      const expirationText = store.getItem(Authenticator.STORE_KEY_EXPIRATION);
+
+      if (!userJSONText) {
+        console.log("ERROR: No user found in stored credentials.");
+      }
+      else if (!expirationText) {
+        console.log("ERROR: No expiration found in stored credentials.");
+      }
+      else {
+        console.log("Loading stored credentials...")
+
+        const userJSON = JSON.parse(userJSONText);
+        const user = User.fromJSON(userJSON);
+
+        const expiration = moment(expirationText);
+
+        this.user = user
+        this.expiration = expiration;
+
+        return;
+      }
+
+      this.user = null;
+      this.expiration = null;
+    }
+  }
+
   login = async (username, credentials) => {
-    console.log("Logging in as " + username + "...");
+    console.log(`Logging in as ${username}...`);
     const result = await this.source.login(username, credentials);
 
     if (result !== null) {
@@ -84,13 +134,14 @@ export class Authenticator {
   }
 
   logout = async () => {
-    console.log("Logging out as " + this.user.username + "...")
+    console.log(`Logging out as ${this.user.username}...`)
     await this.source.logout();
     this.user = null;
     this.expiration = null;
   }
 
   isLoggedIn = () => {
+    this.loadFromStorage();
     return (this.user !== null);
   }
 
--- src/auth.test.js
+++ src/auth.test.js
@@ -1,4 +1,38 @@
+import moment from "moment";
+
 import { Authenticator, TestAuthentationSource, User } from "./auth";
+
+
+expect.extend({
+  toBeAfterMoment(received, other) {
+    if (received.isAfter(other)) {
+      return {
+        message: () => `expected ${received} not to be after ${other}`,
+        pass: true,
+      }
+    }
+    else {
+      return {
+        message: () => `expected ${received} to be after ${other}`,
+        pass: false,
+      }
+    }
+  },
+  toBeSameAsMoment(received, other) {
+    if (received.isSame(other)) {
+      return {
+        message: () => `expected ${received} not to be the same as ${other}`,
+        pass: true,
+      }
+    }
+    else {
+      return {
+        message: () => `expected ${received} to be the same as ${other}`,
+        pass: false,
+      }
+    }
+  },
+});
 
 
 describe("User", () => {
@@ -68,7 +102,7 @@ describe("TestAuthentationSource", () => {
   test("valid login -> expiration", async () => {
     const username = "user";
     const password = username;
-    const now = new Date();
+    const now = moment();
     const source = new TestAuthentationSource();
     const result = await source.login(username, {password: password});
 
@@ -76,7 +110,7 @@ describe("TestAuthentationSource", () => {
 
     const expiration = result.expiration;
 
-    expect(expiration.getTime()).toBeGreaterThan(now.getTime());
+    expect(expiration).toBeAfterMoment(now);
   });
 
   test("invalid login -> null", async () => {
@@ -91,17 +125,104 @@ describe("TestAuthentationSource", () => {
 });
 
 
+function verifyCleanAuthStorage() {
+  const store = window.localStorage;
+
+  // Make sure storage is clear
+  if (store.getItem(Authenticator.STORE_KEY_CLASS)) {
+    throw new Error("Found class in local storage.");
+  }
+  if (store.getItem(Authenticator.STORE_KEY_USER)) {
+    throw new Error("Found user in local storage.");
+  }
+  if (store.getItem(Authenticator.STORE_KEY_EXPIRATION)) {
+    throw new Error("Found expiration in local storage.");
+  }
+}
+
+
+async function populateAuthStorage(username, credentials, source) {
+  if (!username) {
+    username = "Cheese Butter";
+  }
+  if (!credentials) {
+    credentials = {password: username};
+  }
+  if (!source) {
+    source = new TestAuthentationSource();
+  }
+
+  const {user, expiration} = await source.login(username, credentials);
+
+  const store = window.localStorage;
+
+  store.setItem(Authenticator.STORE_KEY_CLASS, source.constructor.name);
+  store.setItem(Authenticator.STORE_KEY_USER, JSON.stringify(user.asJSON()));
+  store.setItem(Authenticator.STORE_KEY_EXPIRATION, expiration.toISOString());
+
+  return {source: source, user: user, expiration: expiration};
+}
+
+
+function cleanAuthStorage() {
+  const store = window.localStorage;
+
+  store.removeItem(Authenticator.STORE_KEY_CLASS);
+  store.removeItem(Authenticator.STORE_KEY_USER);
+  store.removeItem(Authenticator.STORE_KEY_EXPIRATION);
+}
+
+
 describe("Authenticator", () => {
+
+  afterEach(() => {
+    cleanAuthStorage();
+  });
 
   test("authentication source is defined", () => {
     const message = "authentication source is not defined";
+
     expect(() => {new Authenticator()}).toThrow(message);
     expect(() => {new Authenticator(undefined)}).toThrow(message);
   });
 
   test("authentication source is not null", () => {
     const message = "authentication source is null";
+
     expect(() => {new Authenticator(null)}).toThrow(message);
+  });
+
+  test("initial state", () => {
+    const source = new TestAuthentationSource();
+    const authenticator = new Authenticator(source);
+
+    expect(authenticator.source).toBe(source);
+    expect(authenticator.user).toBeNull();
+    expect(authenticator.expiration).toBeNull();
+  });
+
+  test("loadFromStorage, empty", async () => {
+    verifyCleanAuthStorage();
+
+    const source = new TestAuthentationSource();
+    const authenticator = new Authenticator(source);
+
+    authenticator.loadFromStorage();
+
+    expect(authenticator.user).toBeNull();
+    expect(authenticator.expiration).toBeNull();
+  });
+
+  test("loadFromStorage, populated", async () => {
+    verifyCleanAuthStorage();
+
+    const { source, user, expiration } = await populateAuthStorage();
+    const authenticator = new Authenticator(source);
+
+    authenticator.loadFromStorage();
+
+    expect(authenticator.user.asJSON()).toEqual(user.asJSON());
+    expect(authenticator.expiration).toBeSameAsMoment(expiration);
   });
 
   test("valid login -> user", async () => {
@@ -124,7 +245,7 @@ describe("Authenticator", () => {
   test("valid login -> expiration", async () => {
     const username = "user";
     const password = username;
-    const now = new Date();
+    const now = moment();
     const source = new TestAuthentationSource();
     const authenticator = new Authenticator(source);
     const result = await authenticator.login(username, {password: password});
@@ -133,7 +254,7 @@ describe("Authenticator", () => {
 
     const expiration = authenticator.expiration;
 
-    expect(expiration.getTime()).toBeGreaterThan(now.getTime());
+    expect(expiration).toBeAfterMoment(now);
   });
 
   test("invalid login", async () => {
