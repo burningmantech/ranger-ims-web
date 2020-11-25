782c23d9ceab6d990247212ba85f8b79381ff8cb
Wilfredo Sánchez <wsanchez@wsanchez.net>
2020-11-24 20:10:36 -0800

    Factor out event dropdown

--- src/components/nav/NavigationBar.js
+++ src/components/nav/NavigationBar.js
@@ -7,6 +7,7 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 
 import { URL } from "../../URL";
 
+import EventDropdown from "./EventDropdown";
 import LoginDropdown from "./LoginDropdown";
 
 import logo from "../../logo.svg";
@@ -25,21 +26,10 @@ export default class NavigationBar extends Component {
             src={logo}
           />
         </Navbar.Brand>
-
         <Navbar.Toggle aria-controls="responsive_navbar_nav" />
-
         <Navbar.Collapse id="responsive_navbar_nav">
-
-          <Nav className="mr-auto">
-            <NavDropdown title="No Event Selected" id="nav_events_dropdown">
-              <NavDropdown.Item href="#events/2020">2020</NavDropdown.Item>
-              <NavDropdown.Item href="#events/2019">2019</NavDropdown.Item>
-              <NavDropdown.Item href="#events/2018">2018</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-
+          <Nav className="mr-auto"><EventDropdown /></Nav>
           <Nav><LoginDropdown /></Nav>
-
         </Navbar.Collapse>
       </Navbar>
     );
