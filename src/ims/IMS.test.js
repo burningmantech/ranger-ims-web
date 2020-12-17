585736f59b5b02e1cfd35aaae46783aeb576a667
Wilfredo Sánchez <wsanchez@wsanchez.net>
2020-12-16 20:05:40 -0800

    Move User tests

--- src/ims/IMS.test.js
+++ src/ims/IMS.test.js
@@ -1,6 +1,6 @@
 import moment from "moment";
 
-import IncidentManagementSystem from "./IMS";
+import IncidentManagementSystem, { User } from "./IMS";
 import {
   TestIncidentManagementSystem, testIncidentManagementSystem, theBag
 } from "./TestIMS";
@@ -53,6 +53,85 @@ expect.extend({
       pass: true,
     }
   },
+
+});
+
+
+describe("User", () => {
+
+  test("username is required", () => {
+    const message = "username is required";
+
+    expect(() => {new User()}).toThrow(message);
+    expect(() => {new User(undefined, {})}).toThrow(message);
+    expect(() => {new User(null, {})}).toThrow(message);
+  });
+
+  test("credentials is required", () => {
+    const username = "Hubcap";
+    const message = "credentials is required";
+
+    expect(() => {new User(username)}).toThrow(message);
+    expect(() => {new User(username, undefined)}).toThrow(message);
+    expect(() => {new User(username, null)}).toThrow(message);
+  });
+
+  test("credentials.expiration is required", () => {
+    const username = "Hubcap";
+    const message = "credentials.expiration is required";
+
+    expect(() => {new User(username, {})}).toThrow(message);
+  });
+
+  test("toJSON", () => {
+    const username = "Hubcap";
+    const credentials = {
+      number: 1,
+      text: "text",
+      array: [1, 2, 3],
+      dict: {a: 1},
+      expiration: moment(),
+    };
+    const user = new User(username, credentials);
+    const userJSON = {username: username, credentials: credentials};
+    const result = user.toJSON();
+
+    expect(result).toEqual(userJSON);
+  });
+
+  test("fromJSON", () => {
+    const username = "Hubcap";
+    const credentials = {
+      number: 1,
+      text: "text",
+      array: [1, 2, 3],
+      dict: {a: 1},
+      expiration: moment(),
+    };
+    const user = new User(username, credentials);
+    const userJSON = user.toJSON();
+    const result = User.fromJSON(userJSON);
+    const resultJSON = result.toJSON();
+
+    expect(resultJSON).toEqual(userJSON);
+  });
+
+  test("JSON round-trip through text", () => {
+    const username = "Hubcap";
+    const credentials = {
+      number: 1,
+      text: "text",
+      array: [1, 2, 3],
+      dict: {a: 1},
+      expiration: moment(),
+    };
+    const user = new User(username, credentials);
+    const userJSONText = JSON.stringify(user.toJSON());
+    const result = User.fromJSON(JSON.parse(userJSONText));
+    const resultJSON = result.toJSON();
+
+    expect(JSON.stringify(resultJSON)).toEqual(userJSONText);
+  });
 
 });
 
