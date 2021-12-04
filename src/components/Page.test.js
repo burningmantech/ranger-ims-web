d343f934d7abcccb1046e11a263921fe14b4cf6b
Wilfredo Sánchez <wsanchez@wsanchez.net>
2021-12-04 11:39:27 -0800

    Unused import

--- src/components/Page.test.js
+++ src/components/Page.test.js
@@ -1,6 +1,5 @@
 import "@testing-library/jest-dom/extend-expect";
 import { act, screen } from "@testing-library/react";
-import userEvent from '@testing-library/user-event';
 
 import {
   renderWithIMSContext, testIncidentManagementSystem
