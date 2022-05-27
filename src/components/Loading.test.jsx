81394c7d32a5abc4f409575f68b9a1b3e8ae3984
Wilfredo Sánchez <wsanchez@wsanchez.net>
2022-05-27 12:15:59 -0700

    Add args to Loading component that will allow it to render its children after loading.

--- src/components/Loading.test.jsx
+++ src/components/Loading.test.jsx
@@ -4,9 +4,70 @@ import { render, screen } from "@testing-library/react";
 import Loading from "./Loading";
 
 describe("Loading component", () => {
-  test("render", () => {
+  test("no args", () => {
     render(<Loading />);
 
     expect(screen.queryByText("Loading…")).toBeInTheDocument();
+  });
+
+  test("condition false", () => {
+    render(<Loading condition={false}>Hello</Loading>);
+
+    expect(screen.queryByText("Loading…")).toBeInTheDocument();
+    expect(screen.queryByText("Hello")).not.toBeInTheDocument();
+  });
+
+  test("condition true", () => {
+    render(<Loading condition={true}>Hello</Loading>);
+
+    expect(screen.queryByText("Loading…")).not.toBeInTheDocument();
+    expect(screen.queryByText("Hello")).toBeInTheDocument();
+  });
+
+  test("condition true with what", () => {
+    render(
+      <Loading condition={true} what={"fluff"}>
+        Hello
+      </Loading>
+    );
+
+    expect(screen.queryByText("Loading fluff…")).not.toBeInTheDocument();
+    expect(screen.queryByText("Hello")).toBeInTheDocument();
+  });
+
+  test("condition true, error true", () => {
+    render(
+      <Loading condition={true} error={true}>
+        Hello
+      </Loading>
+    );
+
+    expect(screen.queryByText("Loading…")).not.toBeInTheDocument();
+    expect(screen.queryByText("Hello")).toBeInTheDocument();
+    expect(screen.queryByText("Failed to load.")).not.toBeInTheDocument();
+  });
+
+  test("condition false, error true", () => {
+    render(
+      <Loading condition={false} error={true}>
+        Hello
+      </Loading>
+    );
+
+    expect(screen.queryByText("Loading…")).not.toBeInTheDocument();
+    expect(screen.queryByText("Hello")).not.toBeInTheDocument();
+    expect(screen.queryByText("Failed to load.")).toBeInTheDocument();
+  });
+
+  test("condition false, error true with what", () => {
+    render(
+      <Loading condition={false} error={true} what={"fluff"}>
+        Hello
+      </Loading>
+    );
+
+    expect(screen.queryByText("Loading…")).not.toBeInTheDocument();
+    expect(screen.queryByText("Hello")).not.toBeInTheDocument();
+    expect(screen.queryByText("Failed to load fluff.")).toBeInTheDocument();
   });
 });
