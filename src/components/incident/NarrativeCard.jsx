ad0f1c43d328ea6e537473ff0eb522af833b1fac
Wilfredo Sánchez <wsanchez@wsanchez.net>
2023-09-24 15:51:30 -0700

    Add Show System Entries toggle

--- src/components/incident/NarrativeCard.jsx
+++ src/components/incident/NarrativeCard.jsx
@@ -1,5 +1,8 @@
 import invariant from "invariant";
 
+import { useState } from "react";
+
+import Form from "react-bootstrap/Form";
 import ListGroup from "react-bootstrap/ListGroup";
 
 import Well from "../base/Well";
@@ -8,12 +11,32 @@ import ReportEntry from "./ReportEntry";
 const NarrativeCard = ({ reportEntries }) => {
   invariant(reportEntries != null, "reportEntries property is required");
 
+  const [showSystem, setShowSystem] = useState(false);
+
+  const onShowSystemEntriesChange = (event) => {
+    setShowSystem(!showSystem);
+  };
+
   return (
-    <Well id="incident_narrative_card" title="Incident Narrative">
-      {reportEntries.map((reportEntry, index) => [
-        <ReportEntry reportEntry={reportEntry} />,
-      ])}
-    </Well>
+    <>
+      <Well id="incident_narrative_card" title="Incident Narrative">
+        <Form className="d-flex flex-row-reverse">
+          <Form.Check
+            type="switch"
+            id="narrative-show-system-entries"
+            label="Show System Entries"
+            reverse
+            value={showSystem}
+            onChange={onShowSystemEntriesChange}
+          />
+        </Form>
+        {reportEntries
+          .filter((reportEntry) => showSystem || !reportEntry.systemEntry)
+          .map((reportEntry, index) => [
+            <ReportEntry key={index} reportEntry={reportEntry} />,
+          ])}
+      </Well>
+    </>
   );
 };
 
