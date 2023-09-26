e1b560fcca3bf18093f5047dca593e4a3159ef45
Wilfredo Sánchez <wsanchez@wsanchez.net>
2023-09-26 13:44:30 -0700

    text cleanup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -74,21 +74,23 @@ const Login = ({ children }) => {
             <ModalBody>
               {Error}
 
+              <Form.Text muted>
+                Email and password must match your Ranger Secret Clubhouse
+                profile.
+              </Form.Text>
+
               <Form.Group controlId="username_field">
-                <Form.Label>Ranger Handle or Email</Form.Label>
+                <Form.Label>Registered Email</Form.Label>
                 <Form.Control
                   autoComplete="username email"
                   inputMode="latin-name"
                   minLength="1"
                   onChange={onUsernameChange}
-                  placeholder="Bucket"
+                  placeholder="hubcap@example.com"
                   required={true}
                   type="text"
                   value={username}
                 />
-                <Form.Text muted>
-                  Must match your Ranger Secret Clubhouse profile.
-                </Form.Text>
               </Form.Group>
 
               <Form.Group controlId="password_field">
